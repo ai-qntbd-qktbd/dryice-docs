5ad7ee209b75083e33bafb9e840f3a99a4de22cf
noname0310 <hjnam2014@gmail.com>
2024-10-26 03:12:04 +0900

    add sitemap

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,6 +52,11 @@ const config: Config = {
                 theme: {
                     customCss: "./src/css/custom.css",
                 },
+                sitemap: {
+                    lastmod: "date",
+                    changefreq: "weekly",
+                    priority: 0.5,
+                },
             } satisfies Preset.Options,
         ],
     ],
